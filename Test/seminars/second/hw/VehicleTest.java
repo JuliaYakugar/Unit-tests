6695b2a877f73c366a6caf4167760ed7834fc8f9
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2023-11-18 02:43:46 +0400

    corrections Less_02

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -6,42 +6,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.*;
 
 public class VehicleTest {
+    Car car = new Car("Lada", "Vesta", 2020);
+    Motorcycle motorcycle = new Motorcycle("Yamaha", "FZ1", 2009);
 
     @Test
     public void testCarInVehicle() {
-        Car car = new Car("Lada", "Vesta", 2020);
         assertTrue(car instanceof Vehicle);
     }
 
     @Test
     public void testCar4numWheels() {
-        Car car = new Car("Lada", "Vesta", 2020);
         assertEquals(4, car.getNumWheels());
     }
 
     @Test
     public void testMotorcycle2numWheels() {
-        Motorcycle motorcycle = new Motorcycle("Yamaha", "FZ1", 2009);
         assertEquals(2, motorcycle.getNumWheels());
     }
 
     @Test
     public void testCarTestSpeed() {
-        Car car = new Car("Lada", "Vesta", 2020);
         car.testDrive();
         assertEquals(60, car.getSpeed());
     }
 
     @Test
     public void testMotorcycleTestSpeed() {
-        Motorcycle motorcycle = new Motorcycle("Yamaha", "FZ1", 2009);
         motorcycle.testDrive();
         assertEquals(75, motorcycle.getSpeed());
     }
 
     @Test
     public void testCarPark() {
-        Car car = new Car("Lada", "Vesta", 2020);
         car.testDrive();
         car.park();
         assertEquals(0, car.getSpeed());
@@ -49,7 +45,6 @@ public class VehicleTest {
 
     @Test
     public void testMotorcyclePark() {
-        Motorcycle motorcycle = new Motorcycle("Yamaha", "FZ1", 2009);
         motorcycle.testDrive();
         motorcycle.park();
         assertEquals(0, motorcycle.getSpeed());
